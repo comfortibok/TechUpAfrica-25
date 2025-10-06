fb6a44fb99b7407e2823c0aa7806f05d3e42a402
comfortibok <ibokcomfort93@gmail.com>
2025-10-06 10:21:55 +0100

    Updated

--- componentLibrary/src/components/WithNoPic.jsx
+++ componentLibrary/src/components/WithNoPic.jsx
@@ -5,7 +5,7 @@ export default function WithNoPic() {
     <div className={styles.withNoPicWrap}>
       <img src={TestimonialLogo} alt="" />
 
-      <p className={styles.decription}>
+      <p className={styles.description}>
         “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita
         voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed
         rerum et corporis.”
